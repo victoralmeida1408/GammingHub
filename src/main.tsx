f3490076c8041e52393021c29e8ff6ea3cd221df
fabio.yamaya <fabio.yamaya@gmail.com>
2025-05-19 16:10:24 -0300

    chore: update shadcn dependency

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Login from './login.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Login from "./Login";
 
-createRoot(document.getElementById('root')!).render(
-<StrictMode>
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
     {/* <App /> 
     <CardGame /> */}
     {/* <Navbar /> */}
     <Login />
-  </StrictMode>,
-  
-)
+  </StrictMode>
+);
