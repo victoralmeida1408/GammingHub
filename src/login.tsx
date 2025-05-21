8a0834bb54488bd8fa3a44637a22cce983232eb6
victor almeida <162771621+victoralmeida1408@users.noreply.github.com>
2025-05-21 18:48:57 -0300

    dividi a tela de login

--- src/login.tsx
+++ src/login.tsx
@@ -1,28 +1,38 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { IoFingerPrintSharp } from "react-icons/io5";
 import imagemfundo from './assets/img/avatar.jpg';
 
 function Login() {
     return (
-    <>
-        <img src={imagemfundo} alt="" width={90} height={90}></img>
-        <i> <IoFingerPrintSharp size={96}/>  </i>
+    <><div className="flex h-screen">
+        <div className="flex-1 flex items-center justify-left">
+        <img src={imagemfundo} alt="" width={1000} height={1000}></img>
+       </div>
+
+       <div className="flex-1 flex items-center justify-center"> 
+       
+       <i> <IoFingerPrintSharp size={96}/>  </i>
         <p>login</p>
         <span> faca o login para se identificar criar seus eventos e compartilhar suas fotosa </span>
         
-        <Input ></Input> 
-
+        <Input type="email" id="email" required placeholder="Email"></Input> 
+        <Input type="password" id="password" required placeholder="senha"></Input> 
         <div className="container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}> 
-        <button type="button"> Entrar</button>
+        <Button variant="outline">Entrar</Button>
         </div>
         <div className="container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        <button type="button"> Criar Conta</button> 
+        <Button variant="outline">Criar Conta</Button>
         </div>
         <div className="container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <input type="checkbox" />
         <label>permanecer conectado</label> 
         </div>
         <a className="container" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} href="#">esqueci a senha</a>
+        </div>
+            
+
+        </div>
     </>
     )
 
