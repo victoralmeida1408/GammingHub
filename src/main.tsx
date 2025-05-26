e8a2b274439dc1c8057ff24449ee110ba453a0d9
Guilherme Lima <guilherme_limadavid@yahoo.com.br>
2025-05-26 17:32:05 -0300

    add cardgame

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Login from "./login";
 import SearchBar from "./search";
+import CardGame from "./Cardgame";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> 
-    <CardGame /> */}
+    {/* <App />  */}
+    <CardGame />
     {/* <Navbar /> */}
-    <Login />
+    {/* <Login /> */}
     {/* <SearchBar /> */}
+
   </StrictMode>
 );
